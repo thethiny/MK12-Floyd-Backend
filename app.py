fcd7a7e1dcf448d16091f8f67c69d98163f66e92
thethiny <thethiny@ymail.com>
2025-01-30 17:25:11 +0400

    Support gunicorn and new routes

--- app.py
+++ app.py
@@ -1,56 +1,66 @@
-import yaml
 import os
-from flask import Flask, request, jsonify
+is_windows = os.name == "nt"
+from threading import Lock
+from flask import Flask, request, jsonify, url_for, g
+if not is_windows:
+    import gevent.monkey
+    gevent.monkey.patch_all()
 
 from src.utils.floyd import get_floyd_data, get_floyd_maps, parse_floyd_data
-
-try:
-    with open("secrets.yaml", encoding="utf-8") as f:
-        secrets = yaml.safe_load(f)
-        steam_key = secrets["creds"]["steam"]
-        mk12_key = secrets["keys"]["mk"]
-        wb_key = secrets["keys"]["wb"]
-        os.environ.update(
-            {
-                "MK12_API_KEY": mk12_key,
-                "WB_API_KEY": wb_key,
-            }
-        )
-        # wb_refresh = secrets["creds"]["wb"]
-except FileNotFoundError:
-    steam_key = os.environ.get("STEAM_KEY")
-    if not steam_key:
-        raise ValueError(f"`steam_key` secret missing!")
+from src.utils import init_secrets
+steam_key, *_ = init_secrets()
 
 from src.api.mk12 import MK12API
 from src.api.user_ids import get_psn_user_id, get_steam_user_id
 from src.api.wb import WBAPI
 from src.routes.platforms import find_any, platform_bp
 
+mk_lock = Lock()
 api = MK12API(steam_key=steam_key)
+api.set_mutex_lock(mk_lock)
 api.login()
+
+wb_lock = Lock()
 wb_api = WBAPI(authorization_code=api.wb_authorization_code) # Auth code is one time use
+wb_api.set_mutex_lock(wb_lock)
 
 app = Flask("Floyd Tracker")
 app.register_blueprint(platform_bp, url_prefix="/platforms")
 
-@app.route("/floyd")
-def main():
+# @app.before_request
+# def load_globals():
+#     g.api = api
+#     g.mk_lock = mk_lock
+#     g.wb_api = wb_api
+#     g.wb_lock = wb_lock
+
+def sanitize_platform(platform: str):
+    platform = platform.strip()
+    if platform in ["psn", "ps4"]:
+        platform = "ps5"
+    elif platform in ["xb1", "x360", "wingdk", "xbl"]:
+        platform = "xsx"
+    return platform
+
+@app.route("/id")
+def get_wb_id_route():
     params = request.args
 
-    platform = params.get("platform")
-    username = params.get("username")
+    platform = params.get("platform", "").strip()
+    username = params.get("username", "").strip()
 
     if not username or not platform:
         return jsonify(error="`platform` and `username` are both required!"), 400
 
     if platform == "wb_network":
-        user_id = wb_api.search(username.strip())["public_id"]
+        user_id = wb_api.search(username)["public_id"]
+    elif platform.startswith("wb"):
+        search_by = platform.split("_", 1)[-1]
+        user_id = wb_api.search_by(username, search_by) # friend / incoming / outgoing
+        if user_id:
+            user_id = user_id["public_id"]
     else:
-        if platform in ["psn", "ps4"]:
-            platform = "ps5"
-        elif platform in ["xb1", "x360", "wingdk", "xbl"]:
-            platform = "xsx"
+        platform = sanitize_platform(platform)
         user_id, status_code = find_any()
         if status_code != 200:
             return user_id, status_code # jsonify
@@ -59,9 +69,27 @@ def main():
     if not user_id:
         return jsonify(error=f"Couldn't find user {username}"), 404
 
+    return jsonify({
+        "username": username,
+        "user_id": user_id,
+        "platform": platform,
+    })
+
+@app.get("/data")
+def get_floyd_data_route():
+    user_id = request.args.get("user_id", "")
+    platform = request.args.get("platform", "")
+    username = request.args.get("username", "")
+    if not user_id or not platform or not username:
+        return jsonify(
+            error=f"`user_id`, `platform`, and `username` are required. Info retrieved automatically from {url_for('get_wb_id_route')}"
+        )
+
+    platform = sanitize_platform(platform)
+
     modules = api.get_mk_id_from_wb(user_id, platform)["player_modules"]
     if not len(modules):
-        return jsonify(error=f"User {username} was found but no id was returned from mk servers. If you're on Nintendo Switch, sorry."), 404
+        return jsonify(error=f"User found but no id was returned from mk servers. If you're on Nintendo Switch, sorry."), 404
 
     player_module = modules[0]
     hydra_id = player_module["hydra_id"]
@@ -91,9 +119,19 @@ def main():
         "raw": {floyd_map[f"profilestat{k}"]: v for k, v in parsed_data["raw"].items()},
         "hints": parsed_data["hints"],
     }
-    
+
     return jsonify(user=user_obj, data=parsed_data)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    if not is_windows:
+        from gevent.pywsgi import WSGIServer
+        port = int(os.environ.get("PORT", 8080))
+        print(f"WSGI Active on {port} with GEvent")
+        WSGIServer(("0.0.0.0", port), app).serve_forever()
+    else:
+        app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+        
+    
+    
+    # Run with gunicorn -k gevent -w 1 --worker-connections 100 --preload app:app
