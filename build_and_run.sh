0394986fdcd3705e010ac002b0c0b958239cdcdd
thethiny <thethiny@ymail.com>
2025-01-31 15:32:14 +0000

    Proper logging

--- build_and_run.sh
+++ build_and_run.sh
@@ -2,7 +2,7 @@ sock_path=/var/www/socks
 name=mk12apis
 image=mk12_floyd
 
-docker rm -f $name | echo "$name was not up"
+(docker rm -f $name && echo "Stopped running $name") || echo "$name was not up"
 docker build -t $image -f sock.Dockerfile .
 docker run -it -d \
         --name $name \
